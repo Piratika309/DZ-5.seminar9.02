a46e8a862b9511b2a6e4de9eace7f68421e87aa1
Natali <piratika309@mail.ru>
2023-02-17 00:57:02 +0300

    Копирование массива

---
+++ example003/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу копирования массива
+
+int [] array = new int [7];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,10);
+    Console.Write($"{array[i]}\t");
+}
+Console.WriteLine();
+
+int [] copy = new int[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    copy[i] = array[i];
+    Console.Write($"{copy[i]}\t");
+}
